06961c730ff35394d12b3e42f6116130fcda8980
Bhupesh18k <bhupesh.kumar18k@gmail.com>
2026-03-14 12:41:46 +0530

    Create Main19.java

---
+++ Main19.java
@@ -0,0 +1,18 @@
+// INPUT ARRAY
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        int[] arr;
+        arr = new int[5];
+        arr[0] = 11;
+        arr[1] = 22;
+        arr[2] = 55;
+        arr[3] = 66;
+        arr[4] = 89;
+
+        for(int i = 0; i < arr.length; i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+}
