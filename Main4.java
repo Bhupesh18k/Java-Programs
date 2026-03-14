8f0ccbd07cafb1ab69de993e5a9fa8308307ef57
Bhupesh18k <bhupesh.kumar18k@gmail.com>
2026-03-14 12:20:52 +0530

    Create Main4.java

---
+++ Main4.java
@@ -0,0 +1,21 @@
+// SWAP TWO NUMBERS USING THIRD VARIABLE.
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int a = 0, b = 1;
+
+        for(int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+
+        scn.close();
+    }
+}
