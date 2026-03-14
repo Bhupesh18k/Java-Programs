17478306ca6bfe82d199f746c72e9f33c75c245c
Bhupesh18k <bhupesh.kumar18k@gmail.com>
2026-03-14 12:16:31 +0530

    Create Main1.java

---
+++ Main1.java
@@ -0,0 +1,11 @@
+// ADDITON OF STRING AND INTEGER
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = Integer.parseInt(scn.nextLine());
+        String a = scn.nextLine();
+        scn.close();
+        System.out.print(n + a);
+    }
+}
