5121ea4037a4260ac6e1cb2f9f4efc7324667e58
Bhupesh18k <bhupesh.kumar18k@gmail.com>
2026-03-14 13:04:50 +0530

    Create Main29.java

---
+++ Main29.java
@@ -0,0 +1,33 @@
+// SUM OF EACH SUBARRAY OF ARRAY
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int[] a=new int[n];
+
+        for(int i=0;i<n;i++)
+        {
+            a[i]=scn.nextInt();
+        }
+
+        for(int i=0;i<a.length;i++)
+        {
+            for(int j=i;j<a.length;j++)
+            {
+                int s=0;
+
+                for(int k=i;k<=j;k++)
+                {
+                    s = s + a[k];
+                }
+
+                System.out.println(s);
+            }
+
+            System.out.println();
+        }
+
+        scn.close();
+    }
+}
